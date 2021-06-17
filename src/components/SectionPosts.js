ec8344a86b0039b1a58bae981b6f195cc2ac57dd
Stackbit Code Editor <projects@stackbit.com>
2021-06-17 21:08:03 +0000

    SectionPosts.js: changed

--- src/components/SectionPosts.js
+++ src/components/SectionPosts.js
@@ -27,7 +27,7 @@ export default class SectionPosts extends React.Component {
                         </div>
                     </header>
                     {excerpt && <p className="post-content">{excerpt}</p>}
-                    
+                    <p className="post-content">
                 </div>
             </article>
         );
